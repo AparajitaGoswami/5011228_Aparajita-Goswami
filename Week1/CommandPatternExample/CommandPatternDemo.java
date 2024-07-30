ac782b3cb5c3313a93ba7985f5dc224da65989a5
AparajitaGoswami <140527197+AparajitaGoswami@users.noreply.github.com>
2024-07-31 00:10:08 +0530

    Add files via upload

---
+++ Week1/CommandPatternExample/CommandPatternDemo.java
@@ -0,0 +1,22 @@
+// CommandPatternDemo.java
+public class CommandPatternDemo {
+    public static void main(String[] args) {
+        // Create the receiver
+        Light light = new Light();
+
+        // Create concrete commands
+        Command lightOn = new LightOnCommand(light);
+        Command lightOff = new LightOffCommand(light);
+
+        // Create the invoker
+        RemoteControl remote = new RemoteControl();
+
+        // Turn the light on
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        // Turn the light off
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
